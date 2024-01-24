d2358bde323364ac29317c983a5b8e7a1a4862c7
Olt Latifi <oltlatifi2003@gmail.com>
2024-01-24 18:40:44 +0100

    Pushing editaboutus.php

--- pages/admin/editaboutus.php
+++ pages/admin/editaboutus.php
@@ -1,35 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php
+
+$title = "| Edit About Us";
+include "../../constants.php";
 include "../../database/connection.php";
+include "../../components/head.php";
 
 session_start();
 
 if (!$_SESSION["role"] || !(strtoupper($_SESSION["role"]) == "ADMIN")) {
-  header("Location: " . PROJECT_URL);
-  die();
+    header("Location: " . PROJECT_URL);
+    die();
 }
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $newContent = $_POST["new_content"];
-    $insertContentSQL = "INSERT INTO about_us (content) VALUES (?)";
-    $params = ["s", $newContent];
-    $result = $db->query($insertContentSQL, $params);
+class AboutUs {
+    private $db;
 
-    if ($result) {
-        header("Location: " . PROJECT_URL . "/about_us.php");
-        die();
+    public function __construct(Database $db) {
+        $this->db = $db;
+    }
+
+    public function getData() {
+        $sql = "SELECT content FROM about_us WHERE id = 1";
+        return $this->db->query($sql)[0]['content'];
+    }
+
+    public function updateData() {
+        $content = $_POST["content"];
+
+        $sql = "UPDATE about_us SET content = ? WHERE id = 1";
+        $params = ["s", $content];
+    
+        $stmt = $this->db->getConn()->prepare($sql);
+        $stmt->bind_param(...$params);
+        $stmt->execute();
     }
 }
+
+$aboutus_obj = new AboutUs($db);
+
+$alert = "";
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $aboutus_obj->updateData();
+    $alert = "About us edited succesfully";
+}
+
+
+include "../../components/adminnavbar.php";
 ?>
 
-<div>
-    <p><?php echo $aboutUsContent; ?></p>
-</div>
+<body class="container">
+    <div style="display: none;" class="alert alert-primary margin-ys">
+        <?php echo $alert ?>
+    </div>
+    <main class="form-layout margin-ys min-height-l">
+        <form id="form" class="form" method="POST">
+            <h1>Edit About Us</h1>
+            <label>
+                New Content:
+                <textarea name="content" rows="8" cols="50"><?php echo $aboutus_obj->getData(); ?></textarea>
+            </label>
+            <button>Submit</button>
+        </form>
+        <div>
+            <img style="margin-left: -175px;" src="<?php echo PROJECT_URL ?>/assets/teamwork.png" alt="Teamwork image">
+        </div>
+    </main>
 
-<!-- Form for updating the "About Us" content -->
-<form method="post">
-    <label>
-        New Content:
-        <textarea name="new_content" rows="4" cols="50"></textarea>
-    </label>
-    <button type="submit">Update Content</button>
-</form>
+</body>
+<script>
+
+    let alert = document.querySelector(".alert");
+
+    if (alert.innerHTML.trim() == "") {
+        alert.classList.remove("show");
+    } else {
+        alert.classList.add("show");
+    }
+
+</script>
+</html>
